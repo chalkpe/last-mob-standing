831947eb0169f474d771b5283fcb0d91104b7e6b
ChalkPE <chalk@chalk.pe>
2017-06-12 21:13:37 +0900

    Add dependencies

--- src/main/kotlin/pe/chalk/bukkit/LastMobStanding.kt
+++ src/main/kotlin/pe/chalk/bukkit/LastMobStanding.kt
@@ -1,7 +1,15 @@
 package pe.chalk.bukkit
 
+import de.robingrether.idisguise.api.DisguiseAPI
+import de.robingrether.idisguise.disguise.Disguise
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.OfflinePlayer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -9,12 +17,23 @@ import org.bukkit.plugin.java.JavaPlugin
  * @since 2017-06-12 08:43
  */
 class LastMobStanding : JavaPlugin(), Listener {
+    lateinit var prefix: String
+    lateinit var api: DisguiseAPI
+    lateinit var attacker: OfflinePlayer
+
     override fun onEnable() {
+        prefix = "${ChatColor.AQUA}[$name]"
         server.pluginManager.registerEvents(this, this)
+        api = Bukkit.getServicesManager().getRegistration(DisguiseAPI::class.java).provider
     }
 
     @EventHandler
-    fun onPlayerJoin() {
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        api.disguise(event.player, Disguise.fromString("pig"))
 
+        event.player.inventory.clear()
+        event.player.inventory.addItem(ItemStack(Material.WOOD_SWORD))
+
+        event.player.sendMessage("$prefix Oh, pig!")
     }
 }
